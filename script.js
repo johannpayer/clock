f290e3876703b995ae3ed2b5821af21bd5f849f8
flamesdev <johannpayer123@gmail.com>
2019-02-10 16:08:46 -0500

    Added a title and an icon & cleaned code

--- script.js
+++ script.js
@@ -3,23 +3,24 @@ window.onload = function () {
     document.body.style.backgroundImage = "url(bg/" +
         Math.floor(new Date().getTime() / (1000 * 60 * 60 * 24) % 102) + ".jpg)";
     clockElement = document.getElementById("clock");
-	updateClock();
-	setInterval(function () {
-		updateClock();
-	}, 1000);
+    updateClock();
+    setInterval(function () {
+        updateClock();
+    }, 1000);
 }
 
-var daysOfTheWeek = [ "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" ];
-var months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+var daysOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 function updateClock() {
-	var time = new Date();
-	var hours = time.getHours().toString();
-	var minutes = time.getMinutes().toString();
-	if (hours >= 12)
+    var time = new Date();
+    var hours = time.getHours().toString();
+    var minutes = time.getMinutes().toString();
+    if (hours >= 12)
         hours -= 12;
-	if (minutes.length == 1)
-		minutes = "0" + minutes;
-	
-	clockElement.innerHTML = hours + ":" + minutes + "<br>"
-        + daysOfTheWeek[time.getDay()] + ", " + months[time.getMonth()] + " " + time.getDate(); 
+    if (minutes.length == 1)
+        minutes = "0" + minutes;
+
+    clockElement.innerHTML = hours + ":" + minutes + "<br>" +
+        daysOfTheWeek[time.getDay()] + ", " + months[time.getMonth()] + " " + time.getDate();
 }
