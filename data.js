43d3356d2ac5619467021eef1a36099b99689cf5
flamesdev <johannpayer123@gmail.com>
2020-07-26 02:01:42 -0400

    Added/removed backgrounds, tweaked code

---
+++ data.js
@@ -0,0 +1,542 @@
+var backgrounds = [
+  {
+    "PhotoId": "1549599273-86affaa16e00",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1549573743-38a364618240",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1549464821-b1a47cb8d654",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1549446754-482a6a36e531",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1549432887-5466eff0ce1c",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1549467821-499949c90f7f",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1486246772917-8947d0f7f541",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1463936575829-25148e1db1b8",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1482569020076-99150e41169b",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1484291470158-b8f8d608850d",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1411599070526e808c923/239f12cc",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1456298964505-ef9e1a638209",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1475082211816-b48baf3f69e3",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1488198107225-7897cb48a0f0",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1520277872024-58b40679ddb4",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1453872302360-eed3c5f8ff66",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1508726295872-0b87b9999406",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1516205651411-aef33a44f7c2",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1509235029464-3a00a743581f",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1433259651738-0e74537aa8b5",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1517364940780-a7a6c32ce58e",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1516026672322-bc52d61a55d5",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1508182390781-8dd476c3237c",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1549241520-425e3dfc01cb",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1448375240586-882707db888b",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1549334707-3aa2dacd0c57",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1420442442888-15c96323ec3c",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1548407260-da850faa41e3",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1548561711-73eae96ad48d",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1547917818-5020d2e79c80",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1547989453-11e67ffb3885",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1491550509181-fa5704644766",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1547828535-340f8a840376",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1510674288907-1b60ac093e87",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1547707981-eeb6a7ac3c25",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1547532182-bf296f6be875",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1547299869-5f213505b8bd",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1547626740-02cb6aed9ef8",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1511284956800-537a4ce2c800",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1549366021-9f761d450615",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1548761208-b7896a6ff225",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1547407139-3c921a66005c",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1547996814-842ca28805fd",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1547716752-9a0331effbef",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1547408999-a6ced14c9d9b",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1548586847-ef68f30601cb",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1549403610-177341eb0f67",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1548539930-b74b69fd4e08",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1536152470836-b943b246224c",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1547582304-f3dba6c9d3cf",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1547822686-8ba163e1122a",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1437422061949-f6efbde0a471",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1447166059867-4c6a7c2f7a2e",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1526577579646-2402908bcb48",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1515266591878-f93e32bc5937",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1547958600-915c8a5131de",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1519681393784-d120267933ba",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1549829454-5bdf2c2f31d6",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1453323403791-7fbd02be3e40",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1529419412599-7bb870e11810",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1421789665209-c9b2a435e3dc",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1455218873509-8097305ee378",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1523712999610-f77fbcfc3843",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1549185856-46f74cfc0d20",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1496660958627-5524cb7c747e",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1503919275948-1f118d8ecf0b",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1455831823533-1e2a1f2dea9b",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1522611378593-de6ba455e19e",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1569402754445-50bc89bc8c4a",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1484312152213-d713e8b7c053",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1569397555232-5f30a3a06f8d",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1569323414408-4983f9a8490b",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1569302911021-297d2362e3d3",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1486728297118-82a07bc48a28",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1568497526806-5506e3bce6fc",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1569521588854-9b461abc92ac",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1569531412913-08101790fb10",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1571845440447-703f22a90d94",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1571842834860-82f303b665a3",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1571821807771-62cf66ac3f14",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1571785194870-35e63128e6e6",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1555217851-6141535bd771",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1552841937-9a625c58f0bd",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1592249797396-311ed9d417f3",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1499260320506-8144c5c33ddf",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1516472096803-187d3339b36f",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1531264993164-04a70aa46453",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1567457399160-08d6ee75ac86",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1592117973983-85fe06ebfd34",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1592146468699-ac0e281c5c37",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1592229127216-2b90ca9cd13d",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1592195241186-a989aa02b9be",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1592254235052-433f97532ef3",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1592320937521-84c88747a68a",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1591959717162-41907f9ee7b4",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1591937630448-1a3a6f194251",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1591880765187-196ec5e1e714",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1592232602259-778d5847f8e8",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1592010092648-59560e2679c8",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1592168680113-5a82658d9383",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1592168630489-c4d755ff8a7b",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1591880765211-b9555f3ddfea",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1591880765599-f0ed6b7c06ff",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1591880765153-ab6a11bafdb7",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1592401883513-fa24b4090580",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1592382419665-40ded771c471",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1592334679391-6e6b0f2fe96c",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1592334454447-7d0aa7feda01",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1577003833619-76bbd7f82948",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1567769541495-338ee7203e3c",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1584483698409-60c493a7b630",
+    "BlackText": true
+  },
+  {
+    "PhotoId": "1544619371-2aacd756277d",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1587678711204-ca6d3d2f1f3b",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1469228252629-cbe7cb7db2c8",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1482003297000-b7663a1673f1",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1513546493312-0066d7de3fd2",
+    "BlackText": false
+  },
+  {
+    "PhotoId": "1501213045308-cfe6e352d850",
+    "BlackText": false
+  },
+	{
+		"PhotoId": "1595681224916-98844d59ea47",
+		"BlackText": true
+	},
+	{
+		"PhotoId": "1595657241488-468423581c23",
+		"BlackText": true
+	},
+	{
+		"PhotoId": "1588099246635-d6868ab08b44",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1595605165365-cb52a719cbd9",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1595683763139-808e8f31b1a7",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1595589982168-77b64bc1b485",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1595584139927-1cc794198ee7",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1595596702299-1cfeca505d85",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1595533768273-e8bee69577ee",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1595590706419-1a57fcce3090",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1595520519880-a86c48ea536c",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1595263541740-cd107dce365d",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1595255967279-ad1d4d8db400",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1594038658121-219eda06322c",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1585335374511-84eb54dc2396",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1569690324072-c6c46dad3194",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1474433188271-d3f339f41911",
+		"BlackText": false
+	},
+	{
+		"PhotoId": "1594882471743-2758d2ce5f00",
+		"BlackText": false
+	},
+]
--- script.js
+++ script.js
@@ -1,64 +1,61 @@
-"use strict";
-
-let backgrounds;
 let clockElement;
 let backgroundSeed;
-$.getJSON("https://raw.githubusercontent.com/flamesdev/clock/master/data.json", function (json) {
-	let args = window.location.href.split("?seed=");
-	if (args.length === 2) {
-		backgroundSeed = args[1];
-	}
 
-	backgrounds = json.Backgrounds;
+$(function () {
+  let args = window.location.href.split("?seed=");
+  if (args.length === 2) {
+    backgroundSeed = args[1];
+  }
 
-	clockElement = document.getElementById("clock");
-	updateBackground();
-	updateClock(true);
-	setInterval(() => updateClock(false), 1000);
+  clockElement = document.getElementById("clock");
+  updateBackground();
+  updateClock(true);
+  setInterval(() => updateClock(false), 1000);
 });
 
 function updateClock(force) {
-	let date = new Date();
-	if (date.getSeconds() === 0 || force) {
-		let hours = date.getHours();
-		let minutes = date.getMinutes();
-		if (hours === 0 && minutes === 0) {
-			updateBackground();
-		}
+  let date = new Date();
+  if (date.getSeconds() === 0 || force) {
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    if (hours === 0 && minutes === 0) {
+      updateBackground();
+    }
 
-		let displayHours = hours % 12;
-		clockElement.innerHTML = (displayHours === 0 ? 12 : displayHours) + ":" + (minutes.toString().length === 1 ? "0" : "") +
-			minutes + " " + (hours < 12 ? "A" : "P") + "M<br>" +
-			date.toLocaleDateString('en-us', {
-				month: 'long',
-				day: 'numeric',
-				weekday: 'long'
-			});
-	}
+    let displayHours = hours % 12;
+    clockElement.innerHTML = (displayHours === 0 ? 12 : displayHours) + ":" + (minutes.toString().length === 1 ? "0" : "") +
+      minutes + " " + (hours < 12 ? "A" : "P") + "M<br>" +
+      date.toLocaleDateString('en-us', {
+        month: 'long',
+        day: 'numeric',
+        weekday: 'long'
+      });
+  }
 }
 
 function updateBackground() {
-	let background = backgrounds[Math.floor(pseudorandom((backgroundSeed === undefined ? new Date().toLocaleDateString('en-us', {
-		year: 'numeric',
-		month: 'numeric',
-		day: 'numeric'
-	}) : backgroundSeed).hashCode()) * backgrounds.length)];
-	document.body.style.backgroundImage = "url(https://images.unsplash.com/photo-" + background.PhotoID + ")";
-	clockElement.style.color = background.BlackText ? "black" : "white";
+  let background = backgrounds[Math.floor(pseudorandom((backgroundSeed === undefined ? new Date().toLocaleDateString('en-us', {
+    year: 'numeric',
+    month: 'numeric',
+    day: 'numeric'
+  }) : backgroundSeed).hashCode()) * backgrounds.length)];
+  document.body.style.backgroundImage = "url(https://images.unsplash.com/photo-" + background.PhotoId + ")";
+  clockElement.style.color = background.BlackText ? "black" : "white";
 }
 
+let seed;
 function pseudorandom(seed) {
-	seed += 0x6D2B79F5;
-	seed = Math.imul(seed ^ seed >>> 15, seed | 1);
-	seed ^= seed + Math.imul(seed ^ seed >>> 7, seed | 61);
-	return ((seed ^ seed >>> 14) >>> 0) / 4294967296;
+  seed += 0x6D2B79F5;
+  seed = Math.imul(seed ^ seed >>> 15, seed | 1);
+  seed ^= seed + Math.imul(seed ^ seed >>> 7, seed | 61);
+  return ((seed ^ seed >>> 14) >>> 0) / 4294967296;
 }
 
 String.prototype.hashCode = function () {
-	let hash = 0;
-	for (let i = 0; i < this.length; i++) {
-		hash = ((hash << 5) - hash) + this.charCodeAt(i);
-		hash |= 0;
-	}
-	return hash;
+  let hash = 0;
+  for (let i = 0; i < this.length; i++) {
+    hash = ((hash << 5) - hash) + this.charCodeAt(i);
+    hash |= 0;
+  }
+  return hash;
 }
