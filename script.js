119f3c3de26ec40f9658f4bdecdd35c46083379a
flamesdev <johannpayer123@gmail.com>
2021-03-05 14:33:48 -0500

    Reformatted code

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-/* global backgrounds document clock window */
 let backgroundSeed;
 let lastUpdateDay;
 
 function hash(string) {
-  const hashes = [ 3735928559, 1103547991 ];
-  [ ...string ].forEach((char) => {
-    // eslint-disable-next-line no-bitwise
-    [ 2654435761, 1597334677 ].forEach((x, i) => { hashes[i] = Math.imul(hashes[i] ^ char.charCodeAt(0), x); });
+  const hashes = [3735928559, 1103547991];
+  [...string].forEach((char) => {
+    [2654435761, 1597334677].forEach((x, i) => {
+      hashes[i] = Math.imul(hashes[i] ^ char.charCodeAt(0), x);
+    });
   });
 
   hashes.forEach((x, i, a) => {
     const other = a[Math.abs(i - 1)];
-    // eslint-disable-next-line no-bitwise
-    a[i] = Math.imul(x ^ (x >>> 16), 2246822507) ^ Math.imul(other ^ (other >>> 13), 3266489909);
+    a[i] =
+      Math.imul(x ^ (x >>> 16), 2246822507) ^
+      Math.imul(other ^ (other >>> 13), 3266489909);
   });
 
-  // eslint-disable-next-line no-bitwise
   return 4294967296 * (2097151 & hashes[1]) + (hashes[0] >>> 0);
 }
 
 function updateBackground() {
-  const background = backgrounds[hash(backgroundSeed || new Date().setHours(0, 0, 0, 0).toString()) % backgrounds.length];
+  const background =
+    backgrounds[
+      hash(backgroundSeed || new Date().setHours(0, 0, 0, 0).toString()) %
+        backgrounds.length
+    ];
   document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-${background.photoId})`;
   clock.style.color = background.showBlackText ? 'black' : 'white';
 }
@@ -34,16 +38,20 @@ function updateClock(doForce) {
     lastUpdateDay = day;
   }
 
-  const minuteTime = (new Date()).setSeconds(0, 0);
+  const minuteTime = new Date().setSeconds(0, 0);
   if (lastMinute !== minuteTime || doForce) {
     const hours = date.getHours();
     const minutes = date.getMinutes();
-    clock.innerHTML = `${hours % 12 || 12}:${minutes < 10 ? '0' : ''}${minutes} ${hours < 12 ? 'A' : 'P'}M<br>${
-      date.toLocaleDateString('en-us', {
-        month : 'long',
-        day : 'numeric',
-        weekday : 'long',
-      })}`;
+    clock.innerHTML = `${hours % 12 || 12}:${
+      minutes < 10 ? '0' : ''
+    }${minutes} ${hours < 12 ? 'A' : 'P'}M<br>${date.toLocaleDateString(
+      'en-us',
+      {
+        month: 'long',
+        day: 'numeric',
+        weekday: 'long',
+      }
+    )}`;
 
     lastMinute = minuteTime;
   }
@@ -51,9 +59,10 @@ function updateClock(doForce) {
 
 window.addEventListener('load', () => {
   const seperator = '?seed=';
-  const param = decodeURI(window.location.href).split('&').find((x) => x.includes(seperator));
+  const param = decodeURI(window.location.href)
+    .split('&')
+    .find((x) => x.includes(seperator));
   if (param) {
-    // eslint-disable-next-line prefer-destructuring
     backgroundSeed = param.split(seperator)[1];
   }
 
